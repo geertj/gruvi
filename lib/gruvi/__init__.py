472e54369cdc0d330e3af0bafdfc6b816662bc82
Geert Jansen <geertj@gmail.com>
2013-01-20 13:09:18 +0100

    Extend copyright to 2013.

--- lib/gruvi/__init__.py
+++ lib/gruvi/__init__.py
@@ -1,10 +1,10 @@
 #
-# This file is part of gruvi. Gruvi is free software available under the terms
-# of the MIT license. See the file "LICENSE" that was provided together with
-# this source file for the licensing terms.
+# This file is part of gruvi. Gruvi is free software available under the
+# terms of the MIT license. See the file "LICENSE" that was provided
+# together with this source file for the licensing terms.
 #
-# Copyright (c) 2012 the gruvi authors. See the file "AUTHORS" for a complete
-# list.
+# Copyright (c) 2012-2013 the gruvi authors. See the file "AUTHORS" for a
+# complete list.
 
 from gruvi.hub import Hub
 from gruvi.greenlet import Greenlet
