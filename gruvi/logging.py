df5190d58a2caf925b5a4f22c74f00b4497afa35
Geert Jansen <geertj@gmail.com>
2013-11-07 13:43:47 +0100

    Fix logging for Python 2.x
    TODO: Investigate why tox did not catch this.

--- gruvi/logging.py
+++ gruvi/logging.py
@@ -49,34 +49,57 @@ def patch_logger(logger):
     logger.findCaller = findCaller
 
 
-class ContextLogger(logging.LoggerAdapter):
+class ContextLogger(object):
     """A logger adapter that prepends a context string to log messages.
     
     It also supports passing arguments via '{}' format operations.
     """
 
-    def __init__(self, logger, context, parent=None):
-        super(ContextLogger, self).__init__(logger, {})
-        self.context = '{}: {}'.format(parent.context, context) \
-                            if parent else context
-        self._debug = logger.isEnabledFor(logging.DEBUG)
+    # This is not based on logging.LoggingAdapter because the 2.x and 3.x
+    # implementations differ quite a bit, which means we would need to
+    # reimplement almost the entire thing anyway.
 
-    def log(self, level, msg, *args, **kwargs):
+    def __init__(self, logger, context, parent=None):
+        self.logger = logger
+        self.context = '{0}: {1}'.format(parent.context, context) \
+                            if parent else context
+
+    def log(self, level, exc, msg, *args, **kwargs):
+        if not self.logger.isEnabledFor(level):
+            return
         current = fibers.current()
         from .util import objref
         prefix = getattr(current, 'name', objref(current))
-        if self._debug:
+        if self.logger.isEnabledFor(logging.DEBUG):
             target = getattr(current, 'target', None)
             if target:
-                prefix += '{}()'.format(compat.getqualname(target))
+                prefix += '{0}()'.format(compat.getqualname(target))
             elif current.parent is None:
                 prefix += '(root)'
             f = sys._getframe(2)
             fname = os.path.split(f.f_code.co_filename)[1]
-            prefix += '@{}:{}:{}'.format(fname, f.f_code.co_name, f.f_lineno)
+            prefix += '@{0}:{1}:{2}'.format(fname, f.f_code.co_name, f.f_lineno)
         if self.context:
-            prefix += '; {}'.format(self.context)
+            prefix += '; {0}'.format(self.context)
         if args or kwargs:
             msg = msg.format(*args, **kwargs)
-        msg = '[{}] {}'.format(prefix, msg)
-        super(ContextLogger, self).log(level, msg)
+        msg = '[{0}] {1}'.format(prefix, msg)
+        self.logger._log(level, msg, (), exc_info=exc)
+
+    def debug(self, msg, *args, **kwargs):
+        self.log(logging.DEBUG, False, msg, *args, **kwargs)
+
+    def info(self, msg, *args, **kwargs):
+        self.log(logging.INFO, False, msg, *args, **kwargs)
+
+    def warning(self, msg, *args, **kwargs):
+        self.log(logging.WARNING, False, msg, *args, **kwargs)
+
+    def error(self, msg, *args, **kwargs):
+        self.log(logging.ERROR, False, msg, *args, **kwargs)
+
+    def critical(self, msg, *args, **kwargs):
+        self.log(logging.CRITICAL, False, msg, *args, **kwargs)
+
+    def exception(self, msg, *args, **kwargs):
+        self.log(logging.ERROR, True, msg, *args, **kwargs)
