2071abe80ea47a5372ec11afa7dff7b74931c548
Geert Jansen <geertj@gmail.com>
2014-02-08 11:37:53 +0100

    [socketpair] make it work on Unix too
    This is sometimes helpful. I needed this in the Bluepass test suite.
    A real socketpair has an empty getpeername() and getsockname().

--- gruvi/socketpair.py
+++ gruvi/socketpair.py
@@ -3,15 +3,27 @@
 # terms of the MIT license. See the file "LICENSE" that was provided
 # together with this source file for the licensing terms.
 #
-# Copyright (c) 2012-2013 the Gruvi authors. See the file "AUTHORS" for a
+# Copyright (c) 2012-2014 the Gruvi authors. See the file "AUTHORS" for a
 # complete list.
 
+from __future__ import absolute_import, print_function
+
+import sys
 import errno
 import socket
 
+__all__ = ['socketpair']
+
+
+if sys.platform.startswith('win'):
+    assert errno.WSAEWOULDBLOCK == errno.EWOULDBLOCK
+
 
 def socketpair(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0):
-    """Emulate the Unix socketpair() function on Windows."""
+    """Emulate the Unix socketpair() syscall by connecting an AF_INET socket.
+
+    This is useful on platforms like Windows that don't have one.
+    """
     # We create a connected TCP socket. Note the trick with setblocking(0)
     # that prevents us from having to create a thread.
     lsock = socket.socket(family, type, proto)
@@ -23,7 +35,7 @@ def socketpair(family=socket.AF_INET, type=socket.SOCK_STREAM, proto=0):
     try:
         csock.connect((addr, port))
     except socket.error as e:
-        if e.errno != errno.WSAEWOULDBLOCK:
+        if e.errno not in (errno.EAGAIN, errno.EWOULDBLOCK, errno.EINPROGRESS):
             lsock.close()
             csock.close()
             raise
